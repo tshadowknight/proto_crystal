1c2ec0dfc5da1a30d31fa622ee53583b3d30ffbd
TSK <rdv260991@gmail.com>
2018-07-21 17:32:04 +0200

    Added demo style shiny determination

--- engine/gfx/color.asm
+++ engine/gfx/color.asm
@@ -12,28 +12,23 @@ CheckShininess:
 	ld l, c
 	ld h, b
 
-; Attack
-	ld a, [hl]
-	and 1 << SHINY_ATK_BIT
-	jr z, .NotShiny
-
 ; Defense
 	ld a, [hli]
 	and $f
 	cp  SHINY_DEF_VAL
-	jr nz, .NotShiny
+	jr c, .NotShiny
 
 ; Speed
 	ld a, [hl]
 	and $f0
 	cp  SHINY_SPD_VAL << 4
-	jr nz, .NotShiny
+	jr c, .NotShiny
 
 ; Special
 	ld a, [hl]
 	and $f
 	cp  SHINY_SPC_VAL
-	jr nz, .NotShiny
+	jr c, .NotShiny
 
 .Shiny:
 	scf
